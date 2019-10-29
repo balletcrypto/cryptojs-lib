f30b01ec09cef4e75884136bb4cef1d7ec509713
Johnson Cai <caiezq@gmail.com>
2019-10-29 15:59:14 +0800

    fix lotSequencePresent variable error

--- test/confirmation_test.js
+++ test/confirmation_test.js
@@ -13,4 +13,14 @@ describe('Test confirmation code with passphrase', function() {
     expect(publicKeyHex).to.equals("048122e638203d94160cbb79ffc68815013631c94ecd2b44a9daba7d4b06b357ef88f12d80db22b9aa01047a601897937d16a64dcb3a3115316ab8c382ce68563a")
   })
 
+  it('get the address2 ', () => {
+    const confirmation = 'cfrm38VXJ22tKoLqTmChreQrd2MEY5Z6qRwGbkxTFnKbHm2ex73kBbm8Ssh6TQoFTKVzL6dNoCS'
+    const passphrase = '0511-FRXU-01MJ-QCAY-MEZ8'
+    const { valid, generatedAddress, publicKeyHex } = validateConfirmation(confirmation, passphrase)
+    expect(valid).to.equals(true)
+    expect(generatedAddress).to.equals("1MwWPJpapWfMyCJuEsp24Aut3M2Yda8xBR")
+    expect(publicKeyHex).to.equals("027c2c66986d3f878521685ea78a310ecf654a3998d104e284949e7ad6b959a731")
+
+  })
+
 })
