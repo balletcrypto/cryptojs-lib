48921b72198b38db9c91ec358138eb0010fae8cd
Silence Tang <silence.tang@mintery.com>
2019-11-14 19:01:05 +0800

    update segwit send

--- src/btc/signer.js
+++ src/btc/signer.js
@@ -7,10 +7,36 @@ import {
   crypto
 } from 'bitcoinjs-lib'
 import bigNumber from 'bignumber.js'
+function concatTypedArrays(a, b) { // a, b TypedArray of same type
+  var c = new (a.constructor)(a.length + b.length);
+  c.set(a, 0);
+  c.set(b, a.length);
+  return c;
+}
+const calculateUTXOSize = (isSegwit, utxos) => {
+ // TODO Better for calculatefee
+ let vsize = 0;
+ vsize += 10;
+ // transaction input size
+ if (isSegwit) {
+  vsize += utxos.length * 68
+ } else {
+  vsize += utxos.length * 148
+ }
+ // transaction output size
+ if (isSegwit) {
+    vsize += 31;
+ } else {
+  vsize += 34;
+ }
+  return vsize
+}
 const ECPairFromWIF = ECPair.fromWIF
-export default (privateKey, utxos, sendAmount, fee, payerAddress, payeeAddress) => {
+export default (privateKey, utxos, sendAmount, feePerByte, payerAddress, payeeAddress) => {
+  const vsize = 0
   const txb = new TransactionBuilder(NETWORKS.bitcoin)
   const isSegwit = (str) => str.startsWith('bc1')
+  const fee = calculateUTXOSize(true, utxos) * feePerByte
   const getUtxoTotalAmount = utxos.length === 1 ?
     utxos[0].amount :
     utxos.reduce((pre, next) => pre.amount + next.amount)
@@ -35,16 +61,16 @@ export default (privateKey, utxos, sendAmount, fee, payerAddress, payeeAddress) 
         .minus(new bigNumber(`${fee}`).valueOf()).toNumber()
     })
   }
-  console.log(outputs)
   getUtxosKeyPair.forEach((utxo, index) => {
-    if (isSegwit) {
+    if (isSegwit(utxo.address)) {
       console.log(utxo.keyPair.publicKey)
       const hash = crypto.hash160(utxo.keyPair.publicKey)
-      hash.unshift([0, hash.length])
-      console.log(`0${hash.length}${hash}`)
-      const scriptPubKey = hash
+      const prefix = new Uint8Array(2)
+      prefix[0] = 0
+      prefix[1] = hash.length
+      const scriptPubKey = concatTypedArrays(prefix, hash)
       console.log(scriptPubKey)
-      txb.addInput(utxo.txid, utxo.txindex, 0xffffffff, scriptPubKey)
+      txb.addInput(utxo.txid, utxo.txindex, 0xffffffff, Buffer.from(scriptPubKey, 'Hex'))
     } else {
       txb.addInput(utxo.txid, utxo.txindex)
     }
@@ -54,8 +80,20 @@ export default (privateKey, utxos, sendAmount, fee, payerAddress, payeeAddress) 
     txb.addOutput(output.address, output.value)
   });
   getUtxosKeyPair.forEach((utxo, index) => {
-    txb.sign(index, utxo.keyPair)
+    let mixIn = {}
+    if (isSegwit(utxo.address)) {
+      mixIn.prevOutScriptType = 'p2wpkh'
+      mixIn.witnessValue = utxo.amount
+    } else {
+      mixIn.prevOutScriptType = 'p2pkh'
+    }
+    txb.sign({
+      vin: index,
+      keyPair: utxo.keyPair,
+      ...mixIn
+    })
   })
-  console.log(txb.toHex())
+  const tx = txb.build()
+  console.log(tx.toHex())
   return txb
 }
